2eb6b56e97d6755000d2c3ffc08b6a68dde21012
phix01 <aslannaysenur022@gmail.com>
2026-03-12 07:29:57 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,19 @@ const nextConfig: NextConfig = {
    images: {
     domains: ["bulbi-public.s3.eu-west-1.amazonaws.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/tr/event/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=30, stale-while-revalidate=60',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
